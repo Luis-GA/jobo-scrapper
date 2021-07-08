a45e90eb52e54a302ef9f3e9f8773d7cbcc0ead9
Luis Gómez <36197267+Luis-GA@users.noreply.github.com>
2021-07-08 15:13:17 +0200

    setup with readme

--- setup.py
+++ setup.py
@@ -2,5 +2,12 @@
 
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+    )
